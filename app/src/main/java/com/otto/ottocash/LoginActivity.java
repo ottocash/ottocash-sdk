351b4f1b69e73b19b240d10eab98bd5eab4f851e
ardihidayat <ardi@clappingape.com>
2020-05-14 14:23:54 +0700

    take out fabric

--- app/src/main/java/com/otto/ottocash/LoginActivity.java
+++ app/src/main/java/com/otto/ottocash/LoginActivity.java
@@ -30,7 +30,6 @@ public class LoginActivity extends SdkActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
